0af35b658538d5e457e91d34498fb59bdbfdab43
Noud Verstijnen <noud.verstijnen@gmail.com>
2024-02-17 22:36:32 +0100

    add todo endpoints

---
+++ src/controllers/todos.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { dbConnection } from "../db";
+
+export const getAll = (_: Request, res: Response) => {
+  dbConnection.query("SELECT * FROM noldys.todos", (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(results);
+    }
+  });
+};
+
+export const create = (req: Request, res: Response) => {
+  dbConnection.query(
+    `INSERT INTO noldys.todos VALUES (DEFAULT, "${req.body.text}", ${req.body.isComplete})`,
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+};
+
+export const update = (req: Request, res: Response) => {
+  dbConnection.query(
+    `UPDATE noldys.todos SET text = "${req.body.text}", isComplete = ${req.body.isComplete} WHERE id = ${req.params.id}`,
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+};
+
+export const remove = (req: Request, res: Response) => {
+  dbConnection.query(`DELETE FROM noldys.todos WHERE id = ${req.params.id}`, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
 import buddies from "./routes/buddies";
 import birthdays from "./routes/birthdays";
+import todos from "./routes/todos";
 
 const baseUrl = "/api";
 
@@ -8,4 +9,6 @@ export default (app: Express) => {
   app.use(`${baseUrl}/buddies`, buddies);
 
   app.use(`${baseUrl}/birthdays`, birthdays);
+
+  app.use(`${baseUrl}/todos`, todos);
 };
